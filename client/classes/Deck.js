9673c2bc35b06698db11952cfcbe2d829e628565
istvzsig <istvanzsigmond@aol.com>
2024-02-13 23:16:56 +0100

    update Deck

--- client/classes/Deck.js
+++ client/classes/Deck.js
@@ -1,19 +1,42 @@
 import Card from "./Card.js";
-
 export default class Deck {
-    constructor() {
+    constructor(width = 0, height = 0, posX = 0, posY = 0) {
+        this.width = width;
+        this.height = height;
+        this.posX = posX;
+        this.posY = posY;
         this.cards = [];
     }
     create(cardImage, len = 55) {
-        while (len--) {
-            this.cards.push(new Card(cardImage, 100))
+        for (let i = 0; i < len; ++i) {
+            const card = new Card(i, cardImage, 100, 100, 100, 100);
+            this.cards.push(card);
+
         }
     }
-    draw(ctx) {
-        let topCard = this.cards[0];
-        topCard.buffer.width = 200;
-        topCard.buffer.height = 200;
-        topCard.buffer.getContext("2d").drawImage(topCard.image, 0, 0, topCard.buffer.width, topCard.buffer.height)
-        ctx.drawImage(topCard.buffer, 250, 200)
+    shuffle(shuffleTimes = 1) {
+        const cards = this.cards;
+        while (shuffleTimes--) {
+            for (let i = 0; i < cards.length; i++) {
+                let r = Math.random() * cards.length | 0;
+                [cards[i], cards[r]] = [cards[r], cards[i]];
+            }
+        }
+    }
+    drawToCenter(context, card) {
+        let centerX = context.canvas.width / 2 - this.width / 2;
+        let centerY = context.canvas.height / 2 - this.height / 2;
+        context.drawImage(card.buffer, centerX, centerY);
+
+    }
+    draw(context) {
+        const topCard = this.cards[0];
+        topCard.buffer.width = this.width;
+        topCard.buffer.height = this.height;
+
+        topCard.buffer.getContext("2d")
+            .drawImage(topCard.image, 0, 0, topCard.buffer.width, topCard.buffer.height);
+
+        this.drawToCenter(context, topCard);
     }
 }
